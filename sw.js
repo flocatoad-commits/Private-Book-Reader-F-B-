44ca2e271b8c9f7f067107d3ae0bfa5d39b30d26
flocatoad-commits <flocatoad@gmail.com>
2026-09-21 09:32:18 +0630

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('book-reader-v3').then((cache) => {
+    caches.open('book-reader-v4').then((cache) => {
       return cache.addAll([
         './index.html',
         './manifest.json'
@@ -14,7 +14,7 @@ self.addEventListener('activate', (e) => {
     caches.keys().then((keys) => {
       return Promise.all(
         keys.map((key) => {
-          if (key !== 'book-reader-v3') {
+          if (key !== 'book-reader-v4') {
             return caches.delete(key);
           }
         })
